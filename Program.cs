eec249d6ab3ad761d570ed00def0d382310c4be2
SGEhren-dev <60278910+SGEhren-dev@users.noreply.github.com>
2021-01-25 10:42:41 -0600

    Starter structure for Twitch UNO Bot
    Added the starting structure for the UNO Bot. Main file is Program.cs, bot definition is in /libs/UNOBot.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TwitchUNO.libs;
+
+namespace TwitchUNO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UNOBot __this = new UNOBot();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ libs/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchLib.Client.Models;
+
+namespace TwitchUNO.libs
+{
+    interface IMessage
+    {
+        static extern string GetMessageBody(ChatMessage str);
+        static extern string GetMessageAuthor(ChatMessage str);
+    }
+
+    class Message : IMessage
+    {
+        public static string GetMessageAuthor(ChatMessage str)
+        {
+            return str.Message;
+        }
+
+        public static string GetMessageBody(ChatMessage str)
+        {
+            return str.Username;
+        }
+    }
+}
